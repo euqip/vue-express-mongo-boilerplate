58f1e8a8248847a1cf4cbb4469127548bab23e40
euqip <benoit.pique@gmail.com>
2017-11-05 11:34:17 +0100

    Added project references

--- client/app/core/components/responsive_header/maindash.vue
+++ client/app/core/components/responsive_header/maindash.vue
@@ -1,6 +1,11 @@
 <template lang="pug">
   div
     h1 Dashboard
+    p These templates are running with twitter Bootstrap 4 beta 2 and some extra css.
+    p Templates are written with Pug, and compiled with Webpack.
+    p this project is started and inspired from 
+      a(href="https://github.com/icebob/vue-express-mongo-boilerplate" target="_blank") vue-express-mongo-boilerplate
+      span  project by icebob and Bootstrap V4 beta 2.
     section.row.text-center.placeholders
       div.col-6.col-sm-3.placeholder
         img.img-fluid.rounded-circle(src="data:image/gif;base64,R0lGODlhAQABAIABAAJ12AAAACwAAAAAAQABAAACAkQBADs=" width="200" height="200" alt="Generic placeholder thumbnail")
