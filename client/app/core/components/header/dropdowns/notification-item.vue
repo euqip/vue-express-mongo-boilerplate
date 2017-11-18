9b465018569c69144a819ff5f978e3121311f6e4
euqip <benoit.pique@gmail.com>
2017-11-18 08:31:53 +0100

    missed dropdowns/notification-item.vue

---
+++ client/app/core/components/header/dropdowns/notification-item.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+  .notification-item
+    .row
+      .col-sm-2
+        img.avatar(:src="getimgurl()")
+      .col-sm-10
+        div
+          strong {{ name }}
+          |  {{ action }}
+          .footer.float-right
+            small.text-muted {{ delay }}
+    .dropdown-divider
+</template>
+
+<script>
+  export default {
+    props: ['name', 'img', 'action', 'delay', 'src'],
+    methods:{
+      getimgurl() {
+        return this.src + this.img
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+	.notification-item {
+    $avatarSize: 4rem;
+    $avatarRadius : 15%;
+		cursor: pointer;
+      .avatar {
+				width: $avatarSize;
+				height: $avatarSize;
+				float: left;
+				border-radius: $avatarRadius;
+      } // .avatar
+  }
+
+</style>
